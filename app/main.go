30360abaca47a1e71c55c5ebd693e07540a17c86
Akshat Agrawal <akshat.agrawal@infra.market>
2026-01-23 14:59:04 +0530

    implementing type

--- app/main.go
+++ app/main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+var builtinCommands = map[string]struct{}{
+	"echo": {},
+	"exit": {},
+	"type": {},
+}
+
 func main() {
 	for {
 		fmt.Print("$ ")
@@ -20,6 +26,13 @@ func main() {
 			os.Exit(0)
 		} else if strings.HasPrefix(command, "echo") {
 			fmt.Println(strings.TrimSpace(strings.TrimPrefix(command, "echo")))
+		} else if strings.HasPrefix(command, "type") {
+			arg := strings.TrimSpace(strings.TrimPrefix(command, "type"))
+			if _, ok := builtinCommands[arg]; ok {
+				fmt.Println(arg + " is a shell builtin")
+			} else {
+				fmt.Println(arg + ": command not found")
+			}
 		} else {
 			fmt.Println(command + ": command not found")
 		}
