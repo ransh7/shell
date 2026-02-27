e74049ec77fb89d71e31fb374d0f4c560ef52981
Akshat Agrawal <akshat.agrawal@infra.market>
2026-02-27 12:28:32 +0530

    locating executable files

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -31,7 +32,12 @@ func main() {
 			if _, ok := builtinCommands[arg]; ok {
 				fmt.Println(arg + " is a shell builtin")
 			} else {
-				fmt.Println(arg + ": not found")
+				path, err := exec.LookPath(arg)
+				if err != nil {
+					fmt.Println(arg + ": not found")
+					continue
+				}
+				fmt.Println(arg + " is " + path)
 			}
 		} else {
 			fmt.Println(command + ": command not found")
